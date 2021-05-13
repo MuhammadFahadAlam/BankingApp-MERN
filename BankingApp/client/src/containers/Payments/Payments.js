5137e64dbbd3db76e414df2f012bf141f86c0500
MuhammadFahadAlam <62588239+MuhammadFahadAlam@users.noreply.github.com>
2021-05-14 00:52:04 +0500

    Completed Payment Container

--- BankingApp/client/src/containers/Payments/Payments.js
+++ BankingApp/client/src/containers/Payments/Payments.js
@@ -39,9 +39,33 @@ function Payments() {
 					<TransactionCard />
 					<TransactionCard />
 				</div>
-				<div className={styles.recepient}></div>
+				<div className={styles.recepient}>
+					<p className={styles.transfer}>Transfer To</p>
+					<div className={styles.transto}>
+						<ProfileCard />
+						<ProfileCard />
+						<ProfileCard />
+						<ProfileCard />
+						<ProfileCard />
+						<ProfileCard />
+						<ProfileCard />
+						<ProfileCard />
+					</div>
+				</div>
 			</div>
-			<div className={styles.right}></div>
+			<div className={styles.right}>
+				<p className={styles.heading}>New Payments</p>
+				<p>From</p>
+				<ProfileCard />
+				<p>To</p>
+				<ProfileCard />
+				<p>Amount</p>
+				<form action='' className={styles.form}>
+					<input type='text' />
+					<button>Add Comment</button>
+					<button type='submit'>Transfer</button>
+				</form>
+			</div>
 		</div>
 	);
 }
